e14713ce4714c4241c8752de68e1b24732268696
Freyashingala <152647083+Freyashingala@users.noreply.github.com>
2025-01-17 16:29:54 +0530

    Delete server directory

--- server/models/Chat.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ChatSchema = new mongoose.Schema({
-  userMessage: { type: String, required: true },
-  aiResponse: { type: String, required: true },
-  date: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Chat', ChatSchema);
